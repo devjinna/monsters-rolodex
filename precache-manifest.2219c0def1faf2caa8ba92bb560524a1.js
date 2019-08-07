fa2d2418f55f62bbcb27bdb68af1d4618a5161f0
Freddie <tha_sailor71@yahoo.com>
2019-08-06 19:47:13 -0500

    Updates

---
+++ precache-manifest.2219c0def1faf2caa8ba92bb560524a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb10a717644efcbdf8506a0498fa274f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae48bd7bb424ebd0221d",
+    "url": "/monsters-rolodex/static/css/main.6f836667.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ae48bd7bb424ebd0221d",
+    "url": "/monsters-rolodex/static/js/main.407fa1d7.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
